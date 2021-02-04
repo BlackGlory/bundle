2e6c793e7266da75e1b6f2014b0583a021c0ac26
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-02-04 06:27:22 +0000

    chore: upgrade dependencies

--- src/find-all-bundles.ts
+++ src/find-all-bundles.ts
@@ -2,10 +2,10 @@ import { findBundle } from './find-bundle'
 import { AsyncIterableOperator } from 'iterable-operator/lib/es2018/style/chaining/async-iterable-operator'
 import { getErrorResultPromise } from 'return-style'
 import { Bundle } from './types'
-import { findAllDirectoryNames } from 'extra-filesystem'
+import { findAllDirnames } from 'extra-filesystem'
 
 export function findAllBundles(path: string): AsyncIterable<Bundle> {
-  return new AsyncIterableOperator(findAllDirectoryNames(path))
+  return new AsyncIterableOperator(findAllDirnames(path))
     .mapAsync(async x => {
       const [err, bundle] = await getErrorResultPromise(findBundle(x))
       if (err) return undefined
